ab2557f7baca41ec136b862f6c49e002a43606d1
Deep Mahur <108973154+DeepMahurr123@users.noreply.github.com>
2025-04-20 15:49:10 +0530

    Update Calculate.java

--- src/java/Calculate.java
+++ src/java/Calculate.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+@WebServlet("/MyServlet")
 
 public class Calculate extends HttpServlet {
     String na,dob;
